72f83362ee3f2e9cce766c21d9bbcfe90de17791
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-12-24 21:51:53 -0800

    ruby version

---
+++ stack-implementation/stack.rb
@@ -0,0 +1,49 @@
+module StackImplementation
+  class Stack
+    def self.stackify(line)
+      stack = new
+      stack.stack = line.split(/\s+/)
+
+      stack.pop until stack.empty?
+      puts
+    end
+
+    attr_writer :stack
+
+    def initialize
+      @stack = nil
+      @accept = true
+    end
+
+    def push(item)
+      @stack.push item
+    end
+
+    def pop
+      item = @stack.pop
+      if @accept
+        print item
+        print ' ' unless empty?
+      end
+      @accept = !@accept
+    end
+
+    def empty?
+      @stack.empty?
+    end
+  end
+
+  def self.run(filename = ARGV[0])
+    File.open(filename) do |file|
+      stack = Stack.new
+      file.each_line do |line|
+        line.strip!
+        next if line.empty?
+
+        Stack.stackify(line)
+      end
+    end
+  end
+end
+
+StackImplementation.run if __FILE__ == $0
