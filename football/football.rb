9dcf597cb2d5cab586905c65deea34298efd4f2e
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2016-01-15 16:21:24 -0800

    attempt football

---
+++ football/football.rb
@@ -0,0 +1,37 @@
+module Football
+  def self.data_to_s(hash)
+    hash.keys.sort.map do |key|
+      values = hash[key].sort.uniq.join(',')
+      "#{key}:#{values};"
+    end
+    .join(' ')
+  end
+
+  def self.parse_line(line)
+    countries = line.split(/\s+\|\s+/)
+    team_hash = Hash.new { |hash, country| hash[country] = [] }
+    countries.each_with_index do |c, i|
+      i = i + 1
+      c.split(/\s+/).each { |team| team_hash[team] << i.to_s }
+    end
+    team_hash
+  end
+
+  def self.fan_lists(line)
+    team_hash = parse_line(line)
+    data_to_s team_hash
+  end
+
+  def self.run(filename = ARGV[0])
+    File.open(filename) do |file|
+      file.each_line do |line|
+        line.strip!
+        next if line.empty?
+
+        puts fan_lists(line)
+      end
+    end
+  end
+end
+
+Football.run if __FILE__ == $0
